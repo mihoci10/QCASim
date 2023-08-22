e7f7104ffa163b2699fade0bf4e198d6174d747b
Miha Krajnc <miha.krajnc8@gmail.com>
2023-08-22 06:14:28 +0200

    Add buffer to stream

--- QCACore/src/QCACore/Utilities/Stream/BufferedStream.h
+++ QCACore/src/QCACore/Utilities/Stream/BufferedStream.h
@@ -1,20 +1,30 @@
 #pragma once
 
 #include <QCACore/Utilities/Stream/IStream.hpp>
+#include <QCACore/Utilities/Container/RingBuffer.hpp>
 
 namespace QCAC {
 
 	template <class T>
 	class BufferedStream : public IStream<T> {
 	public:
+		BufferedStream(size_t bufferCapacity = 1000);
+
 		size_t Seek(int64_t offset, SeekOrigin origin) override;
 
 		size_t Read(T* buffer, size_t count) override;
 		size_t Write(T* buffer, size_t count) override;
 
 	private:
-		std::unique_ptr<T> m_Buffer;
+		RingBuffer<T> m_Buffer;
 
 	};
+
+	template<class T>
+	BufferedStream<T>::BufferedStream(size_t bufferCapacity) :
+		m_Buffer(RingBuffer<T>(bufferCapacity))
+	{
+
+	}
 
 }
