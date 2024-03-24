7bbb890f9f4b41271057bdb74b720bc5d5d202d1
Miha Krajnc <miha.krajnc8@gmail.com>
2024-03-24 16:10:08 +0100

    Add an example to exe

--- qca-sim/src/main.rs
+++ qca-sim/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+
+use qca_core::sim::*;
+use qca_core::sim::bistable::BistableModel;
+use qca_core::datafile::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Please pass the QCA file name as a parameter!");
+        return;
+    }
+
+    let filename = &args[1]; 
+    println!("Selected file: {}", filename);
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let model = Box::new(BistableModel::new());
+    let cells = cells_deserialize(&contents);
+    
+    println!("Running simulation...");
+    let mut simulator = Simulator::new(model, cells);
+    simulator.run();
+
+    println!("Simulation results:");
+    simulator.get_results().iter().enumerate().for_each(|(i, p)| {
+        println!("  Cell {}: {}", i, p);
+    });
 }
