a0b66c79a1e634528848bb351fb88687a3ae2409
Miha Krajnc <miha.krajnc8@gmail.com>
2025-06-15 11:41:16 +0200

    Add generation of architectures

--- scripts/gen_designs.py
+++ scripts/gen_designs.py
@@ -21,7 +21,7 @@ def set_intercell_distance(orig_design: any, side_length: float, spacing: float)
 
 if len(sys.argv) != 3:
     print("First argument needs to be a *.qcd file!")
-    print("Second argument needs to range <start:stop:step>")
+    print("Second argument needs to be range <start:stop:step>")
     sys.exit(1)
 
 filename = sys.argv[1]
@@ -31,22 +31,26 @@ with open(filename, 'r') as design_file:
     base_name = os.path.splitext(os.path.basename(filename))[0]
     design = json.loads(design_file.read())['design']
 
-    arch_id = design['layers'][0]['cell_architecture_id']
-    arch = design['cell_architectures'][arch_id]
-    side_length = arch['side_length']
-    dot_pos = arch['dot_positions'][0]
-    dot_radius = math.sqrt(dot_pos[0] ** 2 + dot_pos[1] ** 2)
-
-    print(f'Architecture:')
-    print(f'  Name: {arch['name']}')
-    print(f'  Side length: {side_length}nm')
-    print(f'  Dot diameter: {arch['dot_diameter']}nm')
-    print(f'  Dot pos radius: {dot_radius}nm')
-
     spacings = range(range_arg[0], range_arg[1] + range_arg[2], range_arg[2])
     print(f'Generating spacing {min(spacings)}..{max(spacings)}')
-    for spacing in spacings:
-        new_design = set_intercell_distance(design, side_length, spacing)
-        with open(f'{OUT_DIR}/{base_name}_{side_length}_{spacing}.qcd', 'w') as new_design_file:
-            new_design_file.write(json.dumps({"design": new_design}))
+
+    architectures = design['cell_architectures']
+    print(f'Scanning for architectures...')
+    for arch_id in architectures:
+        arch = design['cell_architectures'][arch_id]
+        if arch['dot_count'] != 8:
+            continue
+        side_length = arch['side_length']
+        dot_pos = arch['dot_positions'][0]
+        dot_radius = math.sqrt(dot_pos[0] ** 2 + dot_pos[1] ** 2)
+        print(f'  Name: {arch['name']}')
+        print(f'  Side length: {side_length}nm')
+        print(f'  Dot diameter: {arch['dot_diameter']}nm')
+        print(f'  Dot pos radius: {dot_radius}nm')
+        print('=====')
+
+        for spacing in spacings:
+            new_design = set_intercell_distance(design, side_length, spacing)
+            with open(f'{OUT_DIR}/{base_name}_{side_length}_{spacing}.qcd', 'w') as new_design_file:
+                new_design_file.write(json.dumps({"design": new_design}))
     print(f'Generated designs saved to: {os.path.abspath(OUT_DIR)}')
--- scripts/plot_truth.py
+++ scripts/plot_truth.py
@@ -29,18 +29,4 @@ ax1.grid(True, alpha=0.3)
 cbar1 = plt.colorbar(scatter, ax=ax1, shrink=0.8)
 cbar1.set_label('Accuracy', fontsize=12)
 
-# Add some annotations for highest and lowest accuracy points
-max_idx = np.argmax(accuracies)
-min_idx = np.argmin(accuracies)
-ax1.annotate(f'Max: {accuracies[max_idx]:.3f}',
-             xy=(x_coords[max_idx], y_coords[max_idx]),
-             xytext=(10, 10), textcoords='offset points',
-             bbox=dict(boxstyle='round,pad=0.3', facecolor='yellow', alpha=0.7),
-             arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
-ax1.annotate(f'Min: {accuracies[min_idx]:.3f}',
-             xy=(x_coords[min_idx], y_coords[min_idx]),
-             xytext=(10, -20), textcoords='offset points',
-             bbox=dict(boxstyle='round,pad=0.3', facecolor='orange', alpha=0.7),
-             arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
-
-plt.savefig('test.pdf')
+plt.show()
